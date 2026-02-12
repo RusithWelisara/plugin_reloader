6d852cefaea57727d7b1010cc2c8332650223cb4
rusithhack <101166805+RusithWelisara@users.noreply.github.com>
2026-02-12 07:56:43 +0530

    safety upgrade

--- addons/plugin_reloader/reloader_dock.gd
+++ addons/plugin_reloader/reloader_dock.gd
@@ -71,7 +71,7 @@ func reload_plugin(plugin_name:String):
 	plugin_interface.set_plugin_enabled(plugin_name, false)
 	
 	# Wait for plugin to be fully disabled and nodes freed
-	await get_tree().create_timer(0.2).timeout
+	await get_tree().create_timer(1.0).timeout
 
 	# Force reload scripts from disk
 	var plugin_path = "res://addons/" + plugin_name
